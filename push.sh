de657bfc9b3b4f2e1a5370a38385578d687fb5d6
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-19 16:14:06 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Given Cab Agency support two caregories: Normal and Premium"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
