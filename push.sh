e2b0e1bf2894a5cca84776396a9fa8c724c8c704
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-19 14:56:38 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Given Multiple rides should return total fare"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
